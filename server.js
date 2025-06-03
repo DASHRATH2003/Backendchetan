6941d2f3c4301c6fee93ba93dd6342c81e448237
Your Name <your.email@example.com>
2025-06-03 15:24:36 +0530

    new

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:5000',
+    ''
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
